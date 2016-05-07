16e4bb274cb912d91db6f47f6bc89157e57cb1a6
Jack Vine <jackv949@gmail.com>
2016-05-07 22:07:14 +0930

    Basic main menu added.

---
+++ Assets/Scripts/UI/UIFunctions.cs
@@ -0,0 +1,26 @@
+﻿/*
+**  UIFunctions.cs: Contains basic functions to be called from the UI.
+*/
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    //Load scene (with overloads)
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    //Quits the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
