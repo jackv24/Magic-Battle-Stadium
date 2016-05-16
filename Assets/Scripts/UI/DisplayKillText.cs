154a22b2eaa840905f269ef66acff195704088c7
Jack Vine <jackv949@gmail.com>
2016-05-16 09:50:01 +0930

    End of day commit (evicted xD)

---
+++ Assets/Scripts/UI/DisplayKillText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DisplayKillText : MonoBehaviour
+{
+    public float[] opacityLevels;
+
+    private List<string> killStrings = new List<string>();
+
+    private Text killText;
+
+    void Awake()
+    {
+        killText = GetComponent<Text>();
+    }
+
+    public void UpdateKills()
+    {
+        int length = opacityLevels.Length;
+
+        int overflow = killStrings.Count - length;
+        for (int i = overflow; i > 0; i--)
+        {
+            killStrings.RemoveAt(0);
+        }
+
+        string finalString = "";
+
+        for (int i = 0; i < killStrings.Count; i++)
+        {
+            //TODO: Implement opacity
+        }
+    }
+
+    public void AddKill(string playerOneName, string playerTwoName, string weaponName)
+    {
+        string newString = playerOneName + " killed " + playerTwoName + " with " + weaponName;
+
+        killStrings.Add(newString);
+    }
+}
