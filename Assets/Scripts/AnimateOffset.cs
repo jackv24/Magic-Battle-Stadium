6aec6e5b00c2a4ca422dc359ec891b9022f349ce
Jack Vine <jackv949@gmail.com>
2016-08-06 15:54:02 +0930

    Minor changes. Moved name option to pre-game screen.

---
+++ Assets/Scripts/AnimateOffset.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * AnimateOffset.cs - Animates the offset of a meshrenderer's material
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class AnimateOffset : MonoBehaviour
+{
+    //How much to offset the texture each second
+    public Vector2 offset;
+    private Vector2 currentOffset;
+
+    private MeshRenderer rend;
+
+    void Awake()
+    {
+        rend = GetComponent<MeshRenderer>();
+    }
+
+	void Update ()
+    {
+        if (rend)
+        {
+            currentOffset += offset * Time.deltaTime;
+
+            rend.material.SetTextureOffset("_MainTex", currentOffset);
+        }
+	}
+}
